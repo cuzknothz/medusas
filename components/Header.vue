b2a025ecc553fe26f10fee3f30c8c37a266eb8e7
cuongnguyenrz <nguyen.cuong@jvb-corp.com>
2022-04-26 21:52:18 +0700

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+interface PropState {
+  title: string;
+  subTitle: string;
+  haha: boolean;
+}
+const props = defineProps<PropState>();
+onUpdated(() => {
+  console.log("haha");
+});
+</script>
+
+<template>
+  <div class="h-[50px] bg-red-400 text-center">HAHA</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import Header from "~~/components/Header.vue";
+</script>
+
+<template>
+  <Header />
+</template>
